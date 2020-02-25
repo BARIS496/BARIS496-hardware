78c34e34d61c209fcd0545c58a4b92ce311e8f26
Barış Önen <bonen@etu.edu.tr>
2020-02-25 21:44:10 +0300

    edit requests done

--- database.py
+++ database.py
@@ -16,8 +16,16 @@ print("   ")
 headers={'Content-type':'application/json', 'Accept':'application/json'}
 
 URL2 = "https://restservices496.herokuapp.com/addContainer"
-data = {'name':'hardwareeeeelastfornow','type':'dosdsdsg','lng':12221,'lat':114244,'address':'adrsssss','weight':1029.0}
-r2 = requests.post(url = URL2, data=json.dumps(data),headers=headers)
 
-pastebin_url = r2.text 
-print("The pastebin URL is:%s"%pastebin_url) 
+#data = {'name':'hardwareeeeelastfornow','type':'dosdsdsg','lng':12221,'lat':114244,'address':'adrsssss','weight':1029.0}
+#r2 = requests.post(url = URL2, data=json.dumps(data),headers=headers)
+#pastebin_url = r2.text 
+#print("The pastebin URL is:%s"%pastebin_url) 
+
+
+URL3 = "https://restservices496.herokuapp.com/editContainer/164"
+
+data = {'name':'hardwareeeeelastfornow','type':'dosdsdsg','lng':12221,'lat':114244,'address':'adrsssss','weight':12311.0}
+r3 = requests.put(url = URL3, data=json.dumps(data),headers=headers)
+pastebin_url2 = r3.text 
+print("The pastebin URL is:%s"%pastebin_url2) 
