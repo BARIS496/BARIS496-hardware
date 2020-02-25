95bd07879a9c3d3797cf450c0359d9f359e1489c
Barış Önen <bonen@etu.edu.tr>
2020-02-25 21:34:06 +0300

    get and post requests done

--- database.py
+++ database.py
@@ -1,7 +1,23 @@
 import requests
+import json
 URL = "http://restservices496.herokuapp.com/containers"
 id = 1
 PARAMS = {'container_id':id}
 r = requests.get(url = URL, params = PARAMS)
 data = r.json()
 print(data)
+
+print("   ")
+
+print("   ")
+
+print("   ")
+
+headers={'Content-type':'application/json', 'Accept':'application/json'}
+
+URL2 = "https://restservices496.herokuapp.com/addContainer"
+data = {'name':'hardwareeeeelastfornow','type':'dosdsdsg','lng':12221,'lat':114244,'address':'adrsssss','weight':1029.0}
+r2 = requests.post(url = URL2, data=json.dumps(data),headers=headers)
+
+pastebin_url = r2.text 
+print("The pastebin URL is:%s"%pastebin_url) 
