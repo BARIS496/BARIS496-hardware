06293d954d64e1c480bfbf3b9d12eb9363d0f78e
Barış Önen <bonen@etu.edu.tr>
2020-02-28 22:44:30 +0300

    Database simulation set to be sending weight info per 10 sec. to database.

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import time
+import random
 URL = "http://restservices496.herokuapp.com/containers"
 id = 1
 PARAMS = {'container_id':id}
@@ -15,17 +17,24 @@ print("   ")
 
 headers={'Content-type':'application/json', 'Accept':'application/json'}
 
-URL2 = "https://restservices496.herokuapp.com/addContainer"
+#URL2 = "https://restservices496.herokuapp.com/addContainer"
 
-#data = {'name':'hardwareeeeelastfornow','type':'dosdsdsg','lng':12221,'lat':114244,'address':'adrsssss','weight':1029.0}
+#data = {'name':'real container','type':'dosdsdsg','lng':12221,'lat':114244,'address':'adrsssss','weight':0.0}
 #r2 = requests.post(url = URL2, data=json.dumps(data),headers=headers)
 #pastebin_url = r2.text 
 #print("The pastebin URL is:%s"%pastebin_url) 
 
 
-URL3 = "https://restservices496.herokuapp.com/editContainer/164"
+URL3 = "https://restservices496.herokuapp.com/editContainer/311"
 
-data = {'name':'hardwareeeeelastfornow','type':'dosdsdsg','lng':12221,'lat':114244,'address':'adrsssss','weight':12311.0}
-r3 = requests.put(url = URL3, data=json.dumps(data),headers=headers)
-pastebin_url2 = r3.text 
-print("The pastebin URL is:%s"%pastebin_url2) 
+last_time_measured = time.time()
+while True:
+    weight = float("{0:.2f}".format(random.uniform(1, 10)))
+    #print to lcd
+    if int(time.time() - last_time_measured) > 10:
+        data = {'name':'real container','type':'dosdsdsg','lng':12221,'lat':114244,'address':'adrsssss','weight':weight}
+        r3 = requests.put(url = URL3, data=json.dumps(data),headers=headers)
+        pastebin_url2 = r3.text 
+        print("The pastebin URL is:%s"%pastebin_url2) 
+        print(weight)
+        last_time_measured = time.time()
