b2862938296797d4b205d4b19eb1149ba9bcae3a
barisonen <barisonen.1996@gmail.com>
2020-03-30 19:05:47 +0300

    Working on interface cont'd

--- ui.py
+++ ui.py
@@ -1,6 +1,129 @@
 import tkinter
 from tkinter import messagebox
 from tkinter import *
+import lcddriver
+import time
+import RPi.GPIO as GPIO
+import requests
+import json
+import time
+from hx711 import HX711
+from urllib.request import urlopen
+adminWindow = None
+headers={'Content-type':'application/json', 'Accept':'application/json'}
+
+display = lcddriver.lcd()
+
+def adminWindowWeightPut():
+    print('asd')
+    
+#this func will be used if needed
+def long_string(display, text = '', num_line = 1, num_cols = 20):
+    if(len(text) > num_cols):
+        display.lcd_display_string(text[:num_cols],num_line)
+        time.sleep(1)
+        for i in range(len(text) - num_cols + 5):
+            text_to_print = text[i:i+num_cols]
+            display.lcd_display_string(text_to_print,num_line)
+            time.sleep(0.2)
+    else:
+        display.lcd_display_string(text,num_line)
+        display = lcddriver.lcd()
+
+def startHardware():
+    try:
+        display.lcd_display_string("Hi!", 1)
+        display.lcd_display_string(":)", 2)
+        GPIO.setmode(GPIO.BCM)
+
+        hx = HX711(dout_pin=5, pd_sck_pin=6)
+
+        error = hx.zero()
+
+        if error:
+            raise ValueError('Cannot tare the scale')
+
+        reading = hx.get_raw_data_mean()
+        
+        if not reading:
+            print('incorrect data', reading)
+
+
+  
+
+
+
+        input('Place a known weighted object and press <enter>')
+        reading = hx.get_data_mean()
+        if reading:
+            known_weight_grams = input('Weight of the object: ')
+            try:
+                value = float(known_weight_grams)
+
+            except ValueError:
+                print('Incorrect type of value')
+
+            ratio = reading / value
+            hx.set_scale_ratio(ratio)
+            print('Scaling done')
+            long_string(display, "Scaling done", 1)
+            time.sleep(1)
+            display.lcd_display_string("                            ", 1)
+            display.lcd_display_string("                            ", 2)
+        else:
+            raise ValueError('Cannot calculate the incoming value')
+
+
+        print("Starting to measure...")
+        input('Ready? (<Enter>)')
+        URL3 = "https://restservices496.herokuapp.com/editContainer/761"
+        last_time_measured = time.time()
+        
+        while True:
+            wght = hx.get_weight_mean(20)
+            print("%.2f" % wght, 'gr')
+            display.lcd_display_string("Weight:", 1)
+            display.lcd_display_string("                             ", 2)
+            display.lcd_display_string("%5.2f gr"%wght, 2)
+            
+            weight = float("{0:.2f}".format(wght))
+            if int(time.time() - last_time_measured) > 15:
+                html = urlopen("http://ipinfo.io/json").read()
+                data = json.loads(html.decode('utf-8'))
+                IP=data['ip']
+                org=data['org']
+                city = data['city']
+                country=data['country']
+                region=data['region']
+                loc=data['loc']
+                long = loc[:loc.index(',')]
+                lat = loc[loc.index(',')+1:]
+
+                headers={'Content-type':'application/json', 'Accept':'application/json'}
+
+                URL3 = "https://restservices496.herokuapp.com/editContainer/761"
+                #data = {'name':'real container','type':'dosdsdsg','longitude':1,'latitude':1,'address':'a','weight':0, 'ip':'a','city':'a','region':'a','country':'a'}
+                data = {'name':'real container','type':'dosdsdsg','longitude':long,'latitude':lat,'address':'adrsssss','weight':weight, 'ip':IP,'city':city,'region':region,'country':country}
+                r3 = requests.put(url = URL3, data=json.dumps(data),headers=headers)
+                
+
+                w = str(weight)
+                print("Weight : " + w + " gr , Location: " + long + ", " + lat + ", " + city + ", " + country + ", IP: " + IP + " sent to database")
+
+                last_time_measured = time.time()
+  
+    except (KeyboardInterrupt, SystemExit):
+        print('The program ended')    
+            
+
+URL = "http://restservices496.herokuapp.com/containers"
+id = 1
+PARAMS = {'container_id':id}
+r = requests.get(url = URL, params = PARAMS)
+data = r.json()
+print("Existing containers:")
+print(data)
+
 
 class takeInput(object):
 
@@ -48,20 +171,24 @@ class takeInput(object):
 mainWindow = tkinter.Tk()
 
 
-
+       
+    
 mainWindow.attributes('-fullscreen', True)
 
 photo = tkinter.PhotoImage(file = "pic1.png")
 background_label = tkinter.Label(mainWindow, image=photo)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-T = tkinter.Text(mainWindow, height=2, width=26, bg = "navajowhite2")
+T = tkinter.Label(mainWindow, text="Welcome to ZoologicalFooding!\nContainer Id:761", height=2, width=26, bg = "navajowhite2")
 T.configure(font=("Comic Sans MS", 17))
 T.pack()
 T.place(relx = 0.5, rely = 0.2, anchor = CENTER)
-T.insert(tkinter.END, "Welcome to ZoologicalFooding!\n           Container Id:761\n")
-T.configure(state='disabled')
 
+
+
+def eWeightGet(e1):
+    print(e1.get())
+    
 def adminSetup():
     count = 0
     while True:
@@ -77,6 +204,48 @@ def adminSetup():
         else:
             break
     tkinter.messagebox.showinfo("Success", "Logged in succesfully")
+    
+    adminWindow = tkinter.Tk()
+    awindowWidth = 600 
+    awindowHeight = 600 
+   
+    apositionRight = int(adminWindow.winfo_screenwidth()/2 - awindowWidth/2)
+    apositionDown = int(adminWindow.winfo_screenheight()/2 - awindowHeight/2)
+ 
+    adminWindow.geometry("+{}+{}".format(apositionRight, apositionDown))
+    adminWindow.geometry('600x600')
+    adminWindow.title("setup")
+    
+    a1 = tkinter.Button(adminWindow, text='Start the hardware', command=startHardware)
+    a1.place(relx = 0.5, rely = 0.1, anchor = CENTER)
+    
+
+
+    a2 = tkinter.Label(adminWindow, text="Place a known weighted object and press")
+    a2.place(relx = 0.5, rely = 0.25, anchor=CENTER)
+    a3 = tkinter.Button(adminWindow, text='here', command=adminWindowWeightPut)
+    a3.place(relx = 0.5, rely = 0.3, anchor=CENTER)
+       
+    a4 = tkinter.Label(adminWindow, text="Weight of the object:")
+    a4.place(relx = 0.5, rely = 0.45, anchor=CENTER)
+    e1 = tkinter.Entry(adminWindow)
+    e1.place(relx = 0.5, rely = 0.5, anchor=CENTER)
+    a5 = tkinter.Button(adminWindow, text='enter', command = lambda: eWeightGet(e1))
+    a5.place(relx = 0.5, rely = 0.55, anchor=CENTER)
+    
+    a7 = tkinter.Button(adminWindow, text='Complete the setup and start measuring (exits admin mode)', command = lambda: eWeightGet(e1))
+    a7.place(relx = 0.5, rely = 0.7, anchor=CENTER)
+
+    
+    
+    
+#   e1 = tkinter.Entry(adminWindow)
+#   e2 = tkinter.Entry(adminWindow)
+
+#   e1.grid(row=2, column=1)
+#   e2.grid(row=3, column=1)
+
+    
     
 def beforeFilling():
     tkinter.messagebox.showinfo("b", "bb")
